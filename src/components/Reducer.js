6ee1b4b3214573079b6b24fbcdf046da7647614a
decozzfx <m.f.r.ozil@gmail.com>
2021-12-31 13:12:53 +0700

    action type is object, can contain multiple value

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -3,9 +3,9 @@ import React, { useReducer, useState } from 'react'
 function reducer(state, action){
     switch(action.type){
         case 'increment':
-            return state + 1
+            return state + action.payload
         case 'decrement':
-            return state - 1
+            return state - action.payload
         default:
             throw new Error()
     }
@@ -16,9 +16,9 @@ const Reducer = () => {
     const [count, setCount] = useReducer(reducer, 0)
     return (
         <div>
-            <button onClick={() => setCount({type : 'increment'})}>+</button>
+            <button onClick={() => setCount({type : 'increment', payload : 2})}>+</button>
             <span>{count}</span>
-            <button onClick={() => setCount({type : 'decrement'})}>-</button>
+            <button onClick={() => setCount({type : 'decrement', payload : 2})}>-</button>
         </div>
     )
 }
